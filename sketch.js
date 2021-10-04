3d4ff5333b8aa4015dc90e28d746e162a89da8d6
VainaviShrestha <89615611+VainaviShrestha@users.noreply.github.com>
2021-10-04 19:47:06 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -58,9 +58,9 @@ function draw() {
   
   //code to reset the background
 
-   if(path.x > height ){
-    path.x = height/2;
-   }
+   //if(path.x > height ){
+    //path.x = height/2;
+   //}
 
    //if(path.y > height ){
    // path.x = height/2;
@@ -70,9 +70,9 @@ function draw() {
   //   path.y = height;
   // }
 
-  // if(path.y > height ){
-  //   path.y = height/2;
-  // }
+   if(path.y > height ){
+    path.y = height/2;
+   }
   
     createCash();
     createDiamonds();
